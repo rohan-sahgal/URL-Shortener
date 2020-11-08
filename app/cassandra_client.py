55b6b9c7f6749b191c85b1be60b03fdea468b636
Hessamshar <hessamoddin.sharifpour@gmail.com>
2020-11-08 17:21:13 -0500

    Update clients

--- app/cassandra_client.py
+++ app/cassandra_client.py
@@ -12,7 +12,7 @@ class Cassandra_Client:
   def __init__(self, hosts, keyspace):
     self._hosts = hosts
     self._keyspace = keyspace
-    connect(hosts, keyspace)
+    self.connect(hosts, keyspace)
   
   def connect(self, hosts, keyspace):
     cluster = Cluster(hosts)
@@ -20,12 +20,12 @@ class Cassandra_Client:
 
   def insert(self, short_resource, long_resource):
     if self._session == None:
-      connect(self._hosts, self._keyspace)
+      self.connect(self._hosts, self._keyspace)
     self._session.execute(insert_statement, (long_resource, short_resource))
 
   def get(self, short_resource):
     if self._session == None:
-      connect(self._hosts, self._keyspace)
+      self.connect(self._hosts, self._keyspace)
     rows = self._session.execute(select_statement, (short_resource, ))
     if rows:
       return rows[0].long_resource
